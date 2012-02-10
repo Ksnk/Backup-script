b488321bb91d5e93c702af693a8dea591139a86a
Ksnk <sergekoriakin@gmail.com>
2012-02-10 13:50:25 +0400

    Merge branch 'master' of https://github.com/Ksnk/Backup-script
    Conflicts:
    	backup.php

---
+++ test/zodiak.sqlyog.sql
@@ -0,0 +1,23 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.1.40-community 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `Zodiak` (
+	`IdZodiak` int (11),
+	`Zodiak` varchar (150)
+); 
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('1','Овен');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('2','Телец');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('3','Близнецы');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('4','Рак');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('5','Лев');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('6','Дева');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('7','Весы');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('8','Скорпион');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('9','Стрелец');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('10','Козерог');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('11','Водолей');
+insert into `Zodiak` (`IdZodiak`, `Zodiak`) values('12','Рыбы');
